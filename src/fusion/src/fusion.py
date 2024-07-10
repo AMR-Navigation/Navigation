bde235ec527bfb64bab1454cdeb79a5346bd1a46
sghdz019 <hernandezsofia5g@gmail.com>
2024-07-10 15:36:50 -0500

    Added messages_filter

--- src/fusion/src/fusion.py
+++ src/fusion/src/fusion.py
@@ -4,6 +4,7 @@ import rospy
 from messages.msg import *
 from geometry_msgs.msg import PoseWithCovarianceStamped
 import tf.transformations
+import message_filters
 
 from time import * 
 from math import *
@@ -14,73 +15,99 @@ from yolo_fov import *
 
 
 class Fusion:
-	def __init__(self):
-		rospy.init_node("fusion",anonymous=False)
-		rospy.Subscriber("LidarList",UWOList,self.updatelaser)
-		rospy.Subscriber("object_detection_results",detection,self.updatedetections)
-		rospy.Subscriber("amcl_pose", PoseWithCovarianceStamped, self.updatepose)
-		self.frame = rospy.Time.now()																		# for synchronization purposes
+    #This function initilazes the ROS Node and sets up the LidarList, ObjectDetection subscribers. It aslo uses message filter to synch the lidar and YOLO detection messages with sync_callback
+    def __init__(self):
+        rospy.init_node("fusion", anonymous=False)
+        self.laser_sub = message_filters.Subscriber("LidarList", UWOList)		#Laser subscriber
+        self.detection_sub = message_filters.Subscriber("object_detection_results", detection)		#Detection subscriber
+        self.pose_sub = rospy.Subscriber("amcl_pose", PoseWithCovarianceStamped, self.updatepose)	#Pose subscriber
+        
+        # Time synchronizer
+        self.ts = message_filters.ApproximateTimeSynchronizer([self.laser_sub, self.detection_sub], queue_size=10, slop=0.1)
+        self.ts.registerCallback(self.sync_callback)
+        
+        self.frame = rospy.Time.now()  # for synchronization purposes
 
-		# Pose variables
-		self.yaw = 0
-		self.x = 0
-		self.y = 0
+        # Initialize pose data storage variables
+        self.yaw = 0
+        self.x = 0
+        self.y = 0
 
-		# Laser variables
-		self.laserarcs = []
+        # Laser and detection variables
+        self.laserarcs = []
+        self.detectionarcs = []
 
-		self.detectionarcs = []
+	#This function updates the robots current pose based on the amcl_pose topic.
+    def updatepose(self, data):
+        print("Got new pose data")
+        # Set class variables to use in other callbacks
+        self.yaw = tf.transformations.euler_from_quaternion([data.pose.pose.orientation.x,
+                                                             data.pose.pose.orientation.y,
+                                                             data.pose.pose.orientation.z,
+                                                             data.pose.pose.orientation.w])[2]
+        self.x = data.pose.pose.position.x
+        self.y = data.pose.pose.position.y
+        print("Robot Yaw")
+        print("Robot Location X: ", self.x)
+        print("Robot Location Y: ", self.y)
+        self.frame = data.header.stamp  # sync with this probably
 
-		# 
+    def sync_callback(self, lidar_data, detection_data):
+        self.updatelaser(lidar_data)
+        self.updatedetections(detection_data)
+        self.fuse()
+
+    def updatelaser(self, data):
+        print("Got new laser data:")
+        self.laserarcs = []
+        for obj in data.objects:
+            self.laserarcs.append(getarc(obj, self.yaw, self.x, self.y))
+        print("Laserarcs: ", self.laserarcs)
+
+    def updatedetections(self, detection):
+        print("Got new YOLO data")
+
+        x = detection.x
+        y = detection.y
+        width = detection.width
+        height = detection.height
+
+        center_x, center_y = box_center(x, y, width, height)
+        self.detectionarcs.append(calc_angle(center_x, center_y))
+
+        print("Detectionarcs: ", self.detectionarcs)
+
+    # def match_lidar_yolo(self):
+    #     matched_objects = []
+    #     for arc in self.laserarcs:
+    #         min_angle, max_angle = arc
+    #         for angle_x, angle_y in self.detectionarcs:
+    #             if min_angle <= angle_x <= max_angle:
+    #                 matched_objects.append((arc, (angle_x, angle_y)))
+    #                 break
+    #     return matched_objects
+
+    # def process_unmatched_lidar(self, matched_objects):
+    #     matched_lidar_arcs = [match[0] for match in matched_objects]
+    #     unknown_arcs = [arc for arc in self.laserarcs if arc not in matched_lidar_arcs]
+    #     return unknown_arcs
+
+    # def fuse(self):
+    #     matched_objects = self.match_lidar_yolo()
+    #     unknown_arcs = self.process_unmatched_lidar(matched_objects)
+
+    #     # Handle matched objects
+    #     for arc, box in matched_objects:
+    #         # Update object location logic here
+    #         print(f"Matched arc: {arc} with box: {box}")
+
+    #     # Handle unknown objects
+    #     for arc in unknown_arcs:
+    #         # Mark as unknown logic here
+    #         print(f"Unknown arc: {arc}")
 
 
-	def updatelaser(self,data):
-		print("Got new laser data:")
-		self.laserarcs = []
-		for obj in data.objects:
-			self.laserarcs.append( getarc(obj,self.yaw,self.x,self.y) )
-		#print rospy.Time(data.header.stamp.secs,data.header.stamp.nsecs), '|', self.frame
-		print ""
-
-	def updatedetections(self,data):
-		print ("Got new YOLO data")
-		print data.header
-		for detection in data.detections:
-			x = detection.x
-			y = detection.y
-			width = detection.width
-			height = detection.height
-
-			center_x, center_y = box_center(x, y, width, height)
-			self.detectionarcs.append( calc_angle(center_x, center_y) )
-
-		print ""
-
-	def updatepose(self,data):
-		print ("Got new pose data")
-		# Set class variables to use in other callbacks
-		self.yaw = tf.transformations.euler_from_quaternion([data.pose.pose.orientation.x,data.pose.pose.orientation.y,data.pose.pose.orientation.z,data.pose.pose.orientation.w])[2]
-		self.x = data.pose.pose.position.x
-		self.y = data.pose.pose.position.y
-		print ""
-		self.frame = rospy.Time(data.header.stamp.secs,data.header.stamp.nsecs)								# try to sync with this probably
-
-	def fuse(self):
-		pass
-		# CURRENT PLAN:
-		# sync with detections and laser arcs
-		# 
-		# using previously compiled list as posterior,
-		# attempt to update the locations of each object
-		# 
-		# then take all remaining data and,
-		# for every arc in laser arcs:
-		# 		attempt to identify it, if impossible then mark as unknown
-
-
-if __name__=="__main__":
-	F = Fusion()
-	print("Initialized.")
-	while True:
-		F.fuse()
-		sleep(.5)
+if __name__ == "__main__":
+    F = Fusion()
+    print("Initialized.")
+    rospy.spin()
