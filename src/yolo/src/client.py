78a8dff449a7b05960ab5b8a5ef8254ea94ac692
Dan <danevans264@gmail.com>
2024-07-09 21:45:35 -0500

    Added some comments

--- src/yolo/src/client.py
+++ src/yolo/src/client.py
@@ -7,23 +7,50 @@ def inference(image):
 	image = image.encode("base64")
 	print "Sending ",len(image)," bytes"
 
+	# Connect to the socket
 	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	sock.connect((HOST, PORT))
 
+	# Send all the data as packets of length 1024
 	i=1024
-	while i<len(image):
+	while i<len(image):							# loop through in blocks of 1024
 		sock.sendall(image[i-1024:i])
 		resp = sock.recv(1024).decode("utf-8")
 		if resp!="Ok": print "ERROROROROROROOR"
 		i+=1024
-		
-	sock.sendall(image[i-1024:])				# send the rest
+	sock.sendall(image[i-1024:])				# send the remaining data
 	resp = sock.recv(1024).decode("utf-8")
-	if resp!="Ok": print "ERROROROROROROOR"
-	sock.sendall(b"0x00")						# terminate
+	if resp!="Ok": print "ERROROROROROROOR"		
+	sock.sendall(b"0x00")						# signal to terminate
 
+	# Receive classification
 	classification = sock.recv(1024).decode("utf-8")
 	return classification
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 if __name__=="__main__":
 	file = open("person.jpg",'rb')
--- src/yolo/src/server.py
+++ src/yolo/src/server.py
@@ -9,23 +9,35 @@ HOST, PORT = "localhost", 9999
 
 class Server(socketserver.BaseRequestHandler):
 	def handle(self):
-		imagebuffer = []
+		imagebuffer = []							# each packet will be put in here
 		while True:
-			data = self.request.recv(1024)
-			if data==b"0x00": break
-			print(len(data))
+			data = self.request.recv(1024)			# get a packet
+			if data==b"0x00": break					# this means all the image data has been sent, so break out of loop
 
 			imagebuffer.append(data)
-			self.request.sendall("Ok".encode('utf-8'))
+			self.request.sendall("Ok".encode('utf-8'))							# respond
 		print("Received ",len(b"".join(imagebuffer))," bytes")
 
-		image = base64.b64decode(b"".join(imagebuffer))
-		nparr = np.frombuffer(image, np.uint8)
-		frame = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-		identification = json.dumps(process_image_frame(frame))
-		self.request.sendall(identification.encode('utf-8'))
+		# Send the completed image to the model
+		image = base64.b64decode(b"".join(imagebuffer))							# decode it
+		nparr = np.frombuffer(image, np.uint8)									# make it a numpy array
+		frame = cv2.imdecode(nparr, cv2.IMREAD_COLOR)							# encode that array
+		identification = json.dumps(process_image_frame(frame))					# process the frame and make the response json
+		self.request.sendall(identification.encode('utf-8'))					# send the json back to the client
 		print("Done")
 		
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
