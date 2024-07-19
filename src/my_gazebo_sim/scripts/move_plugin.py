53240e65338bc8564abfa86c760cc6109f023769
sghdz019 <hernandezsofia5g@gmail.com>
2024-07-19 13:21:08 -0500

    Fixed jumps and speed

--- src/my_gazebo_sim/scripts/move_plugin.py
+++ src/my_gazebo_sim/scripts/move_plugin.py
@@ -10,8 +10,9 @@ class MovePersonPlugin:
     def __init__(self):
         rospy.init_node('move_person_plugin')
         self.model_name = 'person_standing'
-        self.speed = 0.1  # m/s
-        self.rate = rospy.Rate(10)  # 10 Hz
+        self.base_speed = 0.1  # base speed in m/s
+        self.min_speed = 0.08  
+        self.rate = rospy.Rate(10) 
 
         # Wait for services to become available
         rospy.wait_for_service('/gazebo/get_model_state')
@@ -29,16 +30,13 @@ class MovePersonPlugin:
         self.thread.start()
 
     def define_waypoints(self):
-        # Define waypoints relative to the start position
         self.waypoints = [
             (self.start_position.x + 4.0, self.start_position.y),  # Move forward 4 meters
-            (self.start_position.x + 4.0, self.start_position.y - 3.0),  # Move to the left 5 meters
-            (self.start_position.x + 7.0, self.start_position.y - 3.0),
-            (self.start_position.x + 4.0, self.start_position.y),
-            (self.start_position.x, self.start_position.y)
-
-            # (self.start_position.x + 9.0, self.start_position.y - 5.0),  # Move right 9 meters
-            # # Additional waypoints can be added here
+            (self.start_position.x + 4.0, self.start_position.y - 3.5),  # Move to the right 3.5 meters
+            (self.start_position.x + 9.0, self.start_position.y - 3.5),  # Move forwards 5 meters
+            (self.start_position.x + 4.0, self.start_position.y - 3.5),  # Move backwards 5 meters
+            (self.start_position.x + 4.0, self.start_position.y),        # Move to the left by 3.5 meters
+            (self.start_position.x, self.start_position.y)               # Return to original position
         ]
 
     def get_current_position(self):
@@ -47,7 +45,7 @@ class MovePersonPlugin:
             return model_state.pose.position
         except rospy.ServiceException as e:
             rospy.logerr("Service call failed: %s" % e)
-            return Pose().position  # Return a default position in case of an error
+            return Pose().position  # Returns default position in case of an error
 
     def move_model(self):
         while not rospy.is_shutdown():
@@ -57,17 +55,23 @@ class MovePersonPlugin:
             distance_to_target = ((target_position[0] - current_position.x) ** 2 + 
                                   (target_position[1] - current_position.y) ** 2) ** 0.5
 
+            # Check if model is close to the target and update waypoint
             if distance_to_target < 0.1:
                 self.current_waypoint_index = (self.current_waypoint_index + 1) % len(self.waypoints)
                 target_position = self.waypoints[self.current_waypoint_index]
+                distance_to_target = ((target_position[0] - current_position.x) ** 2 + 
+                                      (target_position[1] - current_position.y) ** 2) ** 0.5
+
+            # Speed control
+            speed = max(self.min_speed, self.base_speed * distance_to_target)
 
             direction_x = (target_position[0] - current_position.x) / distance_to_target
             direction_y = (target_position[1] - current_position.y) / distance_to_target
 
             new_position = Pose()
-            new_position.position.x = current_position.x + direction_x * self.speed / 10.0
-            new_position.position.y = current_position.y + direction_y * self.speed / 10.0
-            new_position.position.z = current_position.z  # Assuming no movement in z direction
+            new_position.position.x = current_position.x + direction_x * speed / 10.0
+            new_position.position.y = current_position.y + direction_y * speed / 10.0
+            new_position.position.z = current_position.z 
 
             model_state = ModelState()
             model_state.model_name = self.model_name
